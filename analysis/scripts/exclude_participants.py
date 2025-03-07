feb6461545c4b1fdf7d3c30cda5c7262357dd10a
Your GitHub username here <congtianyue233@gmail.com>
2025-03-07 07:27:03 -0600

    Prepare a python script to exclude participants

---
+++ analysis/scripts/exclude_participants.py
@@ -0,0 +1,5 @@
+"""
+This script is used to exclude participants from the final main analysis.
+
+For now, I have included all participants in preprocessing and the calculation of all measures. 
+"""
